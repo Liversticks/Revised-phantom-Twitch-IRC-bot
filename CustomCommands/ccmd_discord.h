2688d731f29dbb1292f680f0c3783b49a32c26a6
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-05-08 00:35:45 -0700

    Update ccmd_discord.h

--- CustomCommands/ccmd_discord.h
+++ CustomCommands/ccmd_discord.h
@@ -20,7 +20,7 @@ class Command_Discord : public CustomCommand {
 		}
 
 		virtual void Fire(string input) {
-			string discord = Lib::formatChatMessage("Join the PMD Speedrunning Discord at https://discord.gg/010CVBJffLgZjVlBr.");
+			string discord = Lib::formatChatMessage("I think I have a Discord? https://discord.gg/2xHwDSg. Also, join the PMD Speedrunning Discord at https://discord.gg/010CVBJffLgZjVlBr.");
 			TwitchCommandLimit::fetchInstance().AddCommand(discord);
 		}
 };
